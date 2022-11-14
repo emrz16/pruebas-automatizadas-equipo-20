c3442e1218617ef6739d1d085d28db9f0c325950
CristinaGM4 <m.guapacha@uniandes.edu.co>
2022-11-13 19:44:52 -0500

    More scenaries of Login

--- cypress-escenarios/cypress/e2e/validateGhostAdminLogin.cy.js
+++ cypress-escenarios/cypress/e2e/validateGhostAdminLogin.cy.js
@@ -4,7 +4,25 @@ describe('Validate Ghost Admin Login', () => {
         cy.wait(1000);        
     })
 
+    it('Login without writing an email', () => {
+        cy.get('form').within(() => {
+            cy.get('input[id="identification"]').type('')           
+            cy.get('input[id="password"]').type('cristinauniandes2022!')
+            cy.get('button[id="ember7"]').click()
+            cy.wait(1000)
+        })
+     
+    }) 
 
+    it('Login with incorrect password', () => {
+        cy.get('form').within(() => {
+            cy.get('input[id="identification"]').type('m.guapacha@uniandes.edu.co')           
+            cy.get('input[id="password"]').type('445566')
+            cy.get('button[id="ember7"]').click()
+            cy.wait(1000)
+        })
+     
+    }) 
 
     it('Login without an email domain', () => {
         cy.get('form').within(() => {
